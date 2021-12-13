05612c7f45ef4ac31264b1dd96ce27be66c544d6
dougAriza <douglasariza92@gmail.com>
2021-12-13 20:17:20 -0300

    Bishop

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -22,7 +22,6 @@ public abstract class Piece {
 	
 	public boolean isThereAnyPossibleMove() {
 		boolean[][] mat = possibleMoves();
-		
 		for (int i=0; i<mat.length;i++) {
 			for(int j=0; j<mat.length;j++) {
 				if (mat[i][j]) {
